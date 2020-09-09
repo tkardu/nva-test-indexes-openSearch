d6097d980163ce248c1b192a64b0b971527545a8
svenngjeto <svenn.gjeto@unit.no>
2020-09-09 18:57:45 +0200

    PMD!

--- search-resources-api/src/main/java/no/unit/nva/search/SearchResourcesResponse.java
+++ search-resources-api/src/main/java/no/unit/nva/search/SearchResourcesResponse.java
@@ -1,8 +1,6 @@
 package no.unit.nva.search;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import nva.commons.utils.JacocoGenerated;
-import nva.commons.utils.JsonUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +8,6 @@ import java.util.List;
 @JacocoGenerated
 @SuppressWarnings("PMD.ShortMethodName")
 public class SearchResourcesResponse extends ArrayList<String> {
-
-    private static final ObjectMapper mapper = JsonUtils.objectMapper;
 
     /**
      * Crates a list og strings containing hits from elasticsearch.
