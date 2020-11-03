522ca00044c6f86ef4caeaec3bd08bac932d538b
svenngjeto <svenn.gjeto@unit.no>
2020-11-03 13:49:08 +0100

    Adding functionality to list latest published publications

---
+++ search-resources-api/src/main/java/no/unit/nva/search/ListLatestResourcesApiHandler.java
@@ -0,0 +1,68 @@
+package no.unit.nva.search;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import nva.commons.exceptions.ApiGatewayException;
+import nva.commons.handlers.ApiGatewayHandler;
+import nva.commons.handlers.RequestInfo;
+import nva.commons.handlers.RestRequestHandler;
+import nva.commons.utils.Environment;
+import nva.commons.utils.JacocoGenerated;
+import org.apache.http.HttpStatus;
+import org.slf4j.LoggerFactory;
+
+import static no.unit.nva.search.RequestUtil.getResults;
+import static no.unit.nva.search.RequestUtil.getSearchTerm;
+
+public class ListLatestResourcesApiHandler extends ApiGatewayHandler<Void, SearchResourcesResponse> {
+
+    private final ElasticSearchHighLevelRestClient elasticSearchClient;
+
+    @JacocoGenerated
+    public ListLatestResourcesApiHandler() {
+        this(new Environment());
+    }
+
+    public ListLatestResourcesApiHandler(Environment environment) {
+        this(environment, new ElasticSearchHighLevelRestClient(environment));
+    }
+
+    public ListLatestResourcesApiHandler(Environment environment, ElasticSearchHighLevelRestClient elasticSearchClient) {
+        super(Void.class, environment, LoggerFactory.getLogger(ListLatestResourcesApiHandler.class));
+        this.elasticSearchClient = elasticSearchClient;
+    }
+
+
+    /**
+     * Implements the main logic of the handler. Any exception thrown by this method will be handled by {@link
+     * RestRequestHandler#handleExpectedException} method.
+     *
+     * @param input       The input object to the method. Usually a deserialized json.
+     * @param requestInfo Request headers and path.
+     * @param context     the ApiGateway context.
+     * @return the Response body that is going to be serialized in json
+     * @throws ApiGatewayException all exceptions are caught by writeFailure and mapped to error codes through the
+     *                             method {@link RestRequestHandler#getFailureStatusCode}
+     */
+    @Override
+    protected SearchResourcesResponse processInput(Void input,
+                                                   RequestInfo requestInfo,
+                                                   Context context) throws ApiGatewayException {
+
+        String searchTerm = getSearchTerm(requestInfo);
+        int results = getResults(requestInfo);
+        return elasticSearchClient.searchSingleTerm(searchTerm, results);
+    }
+
+
+    /**
+     * Define the success status code.
+     *
+     * @param input  The request input.
+     * @param output The response output
+     * @return the success status code.
+     */
+    @Override
+    protected Integer getSuccessStatusCode(Void input, SearchResourcesResponse output) {
+        return HttpStatus.SC_OK;
+    }
+}
