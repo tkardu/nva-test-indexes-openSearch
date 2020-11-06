d1ffcc06e4e0715b0502f1b01ba7561d88a63722
svenngjeto <svenn.gjeto@unit.no>
2020-11-06 08:53:54 +0100

    Fatfinger

--- search-resources-api/src/main/java/no/unit/nva/search/SearchResourcesResponse.java
+++ search-resources-api/src/main/java/no/unit/nva/search/SearchResourcesResponse.java
@@ -25,8 +25,7 @@ public class SearchResourcesResponse {
      */
     @JacocoGenerated
     @JsonCreator
-    public SearchResourcesResponse(@JsonProperty("@" +
-            "context") URI context,
+    public SearchResourcesResponse(@JsonProperty("@context") URI context,
                                    @JsonProperty("took") int took,
                                    @JsonProperty("total") int total,
                                    @JsonProperty("hits") List<JsonNode> hits) {
