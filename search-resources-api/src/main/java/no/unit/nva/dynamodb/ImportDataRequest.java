0367e279a94b6feae3e440e66073c0dd724aae3a
svenngjeto <svenn.gjeto@unit.no>
2020-11-12 10:57:45 +0100

    empty constructor for parameters

--- search-resources-api/src/main/java/no/unit/nva/dynamodb/ImportDataRequest.java
+++ search-resources-api/src/main/java/no/unit/nva/dynamodb/ImportDataRequest.java
@@ -2,11 +2,14 @@ package no.unit.nva.dynamodb;
 
 public class ImportDataRequest {
     private String s3bucket;
-    private String s3key;
+    private String s3folderkey;
 
-    public ImportDataRequest(String s3bucket, String s3key) {
+    public ImportDataRequest() {
+    }
+
+    public ImportDataRequest(String s3bucket, String s3folderkey) {
         this.s3bucket = s3bucket;
-        this.s3key = s3key;
+        this.s3folderkey = s3folderkey;
     }
 
     public String getS3bucket() {
@@ -17,11 +20,11 @@ public class ImportDataRequest {
         this.s3bucket = s3bucket;
     }
 
-    public String getS3key() {
-        return s3key;
+    public String getS3folderkey() {
+        return s3folderkey;
     }
 
-    public void setS3key(String s3key) {
-        this.s3key = s3key;
+    public void setS3folderkey(String s3folderkey) {
+        this.s3folderkey = s3folderkey;
     }
 }
