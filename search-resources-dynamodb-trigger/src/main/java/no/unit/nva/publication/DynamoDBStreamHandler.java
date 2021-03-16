fb40f57b5a9e06f813ef9ca4bfe6aabbaa29b525
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-03-16 15:51:13 +0100

    tests for deleting and unknown operation

--- search-resources-dynamodb-trigger/src/main/java/no/unit/nva/publication/DynamoDBStreamHandler.java
+++ search-resources-dynamodb-trigger/src/main/java/no/unit/nva/publication/DynamoDBStreamHandler.java
@@ -3,6 +3,7 @@ package no.unit.nva.publication;
 import static java.util.Objects.nonNull;
 import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
+import java.util.HashSet;
 import java.util.Set;
 import no.unit.nva.events.handlers.DestinationsEventBridgeEventHandler;
 import no.unit.nva.events.models.AwsEventBridgeDetail;
@@ -20,13 +21,21 @@ public class DynamoDBStreamHandler extends DestinationsEventBridgeEventHandler<D
 
     public static final String ERROR_PROCESSING_DYNAMO_DBEVENT_MESSAGE = "Error processing DynamoDBEvent";
     public static final String SUCCESS_MESSAGE = "202 ACCEPTED";
-    public static final String UNKNOWN_OPERATION_ERROR = "Not a known operation";
+    public static final String UNKNOWN_OPERATION_ERROR = "Unknown operation: ";
     public static final String EMPTY_EVENT_NAME_ERROR = "Event name for stream record is empty";
     public static final String INSERT = "INSERT";
     public static final String MODIFY = "MODIFY";
     public static final String REMOVE = "REMOVE";
     public static final Set<String> UPSERT_EVENTS = Set.of(INSERT, MODIFY);
     public static final Set<String> REMOVE_EVENTS = Set.of(REMOVE);
+    public static final Set<String> VALID_EVENTS = validEvents();
+
+    private static Set<String> validEvents() {
+        Set<String> events = new HashSet<>(UPSERT_EVENTS);
+        events.addAll(REMOVE_EVENTS);
+        return events;
+    }
+
     public static final String IDENTIFIER = "identifier";
     public static final String LOG_MESSAGE_MISSING_EVENT_NAME = "StreamRecord has no event name: ";
     public static final String LOG_ERROR_FOR_INVALID_EVENT_NAME = "Stream record with id {} has invalid event name: {}";
@@ -65,6 +74,8 @@ public class DynamoDBStreamHandler extends DestinationsEventBridgeEventHandler<D
     protected String processInputPayload(DynamoEntryUpdateEvent input,
                                          AwsEventBridgeEvent<AwsEventBridgeDetail<DynamoEntryUpdateEvent>> event,
                                          Context context) {
+        validateEvent(input.getUpdateType());
+
         attempt(() -> processEvent(input)).orElseThrow();
 
         return SUCCESS_MESSAGE;
@@ -75,6 +86,12 @@ public class DynamoDBStreamHandler extends DestinationsEventBridgeEventHandler<D
             elasticSearchClient.removeDocumentFromIndex(input.getOldPublication().getIdentifier().toString());
         }
         return null;
+    }
+
+    private void validateEvent(String updateType) {
+        if(!VALID_EVENTS.contains(updateType)){
+            throw new IllegalArgumentException(UNKNOWN_OPERATION_ERROR+updateType);
+        }
     }
 
     private boolean isDeleteEvent(DynamoEntryUpdateEvent input) {
