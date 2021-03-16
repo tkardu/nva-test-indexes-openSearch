0e1cd4aa9610cc7d545779956539ffd11ff6e8ac
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-03-16 08:24:04 +0100

    NP-2318: delete event for non indexed published resources.

--- search-resources-dynamodb-trigger/src/main/java/no/unit/nva/publication/DynamoDBStreamHandler.java
+++ search-resources-dynamodb-trigger/src/main/java/no/unit/nva/publication/DynamoDBStreamHandler.java
@@ -1,17 +1,22 @@
 package no.unit.nva.publication;
 
+import static java.util.Objects.nonNull;
+import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
 import java.util.Set;
-import no.unit.nva.events.handlers.EventHandler;
+import no.unit.nva.events.handlers.DestinationsEventBridgeEventHandler;
+import no.unit.nva.events.models.AwsEventBridgeDetail;
 import no.unit.nva.events.models.AwsEventBridgeEvent;
+import no.unit.nva.model.Publication;
 import no.unit.nva.search.ElasticSearchHighLevelRestClient;
+import no.unit.nva.search.exception.SearchException;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@SuppressWarnings({"PMD.UnusedPrivateField","PMD.SingularField"})
-public class DynamoDBStreamHandler extends EventHandler<DynamoEntryUpdateEvent, String> {
+@SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+public class DynamoDBStreamHandler extends DestinationsEventBridgeEventHandler<DynamoEntryUpdateEvent, String> {
 
     public static final String ERROR_PROCESSING_DYNAMO_DBEVENT_MESSAGE = "Error processing DynamoDBEvent";
     public static final String SUCCESS_MESSAGE = "202 ACCEPTED";
@@ -25,9 +30,9 @@ public class DynamoDBStreamHandler extends EventHandler<DynamoEntryUpdateEvent, 
     public static final String IDENTIFIER = "identifier";
     public static final String LOG_MESSAGE_MISSING_EVENT_NAME = "StreamRecord has no event name: ";
     public static final String LOG_ERROR_FOR_INVALID_EVENT_NAME = "Stream record with id {} has invalid event name: {}";
-    private static final Logger logger = LoggerFactory.getLogger(DynamoDBStreamHandler.class);
     public static final String MISSING_PUBLICATION_STATUS =
-            "The data from DynamoDB was incomplete, missing required field status on id: {}, ignoring entry";
+        "The data from DynamoDB was incomplete, missing required field status on id: {}, ignoring entry";
+    private static final Logger logger = LoggerFactory.getLogger(DynamoDBStreamHandler.class);
     private final ElasticSearchHighLevelRestClient elasticSearchClient;
 
     /**
@@ -56,14 +61,35 @@ public class DynamoDBStreamHandler extends EventHandler<DynamoEntryUpdateEvent, 
         this.elasticSearchClient = elasticSearchRestClient;
     }
 
-
     @Override
-    protected String processInput(DynamoEntryUpdateEvent input, AwsEventBridgeEvent<DynamoEntryUpdateEvent> event,
-                                  Context context) {
+    protected String processInputPayload(DynamoEntryUpdateEvent input,
+                                         AwsEventBridgeEvent<AwsEventBridgeDetail<DynamoEntryUpdateEvent>> event,
+                                         Context context) {
+        attempt(() -> processEvent(input)).orElseThrow();
 
-        return null;
-
+        return SUCCESS_MESSAGE;
     }
 
+    private Void processEvent(DynamoEntryUpdateEvent input) throws SearchException {
+        if (isDeleteEvent(input)) {
+            elasticSearchClient.removeDocumentFromIndex(input.getOldPublication().getIdentifier().toString());
+        }
+        return null;
+    }
 
+    private boolean isDeleteEvent(DynamoEntryUpdateEvent input) {
+        return isPresent(input.getOldPublication()) && notPresent(input.getNewPublication());
+    }
+
+    private boolean notPresent(Publication publication) {
+        return !isPresent(publication);
+    }
+
+    private boolean isPresent(Publication publication) {
+        return nonNull(publication) && nonEmptyPublication(publication);
+    }
+
+    private boolean nonEmptyPublication(Publication publication) {
+        return nonNull(publication.getIdentifier());
+    }
 }
