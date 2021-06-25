ed905ac57b1fcfe135bca173b3638f5cd04e8f2a
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-06-25 12:09:21 +0200

    bugfix: default API scheme

--- search-commons/src/main/java/no/unit/nva/search/constants/ApplicationConstants.java
+++ search-commons/src/main/java/no/unit/nva/search/constants/ApplicationConstants.java
@@ -6,6 +6,7 @@ public final class ApplicationConstants {
 
     public static final String ELASTIC_SEARCH_SERVICE_NAME = "es";
     public static final String ELASTIC_SEARCH_INDEX_REFRESH_INTERVAL = "index.refresh_interval";
+    public static final String DEFAULT_API_SCHEME = "https";
     public static Environment ENVIRONMENT = new Environment();
     public static final String ELASTICSEARCH_REGION = readElasticSearchRegion();
     public static final String ELASTICSEARCH_ENDPOINT_API_SCHEME = readElasticSearchEndpointApiScheme();
@@ -21,7 +22,7 @@ public final class ApplicationConstants {
     }
 
     private static String readElasticSearchEndpointApiScheme() {
-        return ENVIRONMENT.readEnv("ELASTICSEARCH_ENDPOINT_API_SCHEME");
+        return ENVIRONMENT.readEnvOpt("ELASTICSEARCH_ENDPOINT_API_SCHEME").orElse(DEFAULT_API_SCHEME);
     }
 
     private static String readElasticSearchEndpointAddress() {
