283cd7f5f143ac43d99ece011d631255b660319f
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-11-19 14:35:49 +0100

    NP-3021: import optimization

--- batch-index/src/main/java/no/unit/nva/search/EmitEventUtils.java
+++ batch-index/src/main/java/no/unit/nva/search/EmitEventUtils.java
@@ -1,16 +1,13 @@
 package no.unit.nva.search;
 
+import static no.unit.nva.search.BatchIndexingConstants.BATCH_INDEX_EVENT_BUS_NAME;
 import com.amazonaws.services.lambda.runtime.Context;
+import java.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsRequest;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsRequestEntry;
-
-import java.time.Instant;
-
-import static no.unit.nva.search.BatchIndexingConstants.BATCH_INDEX_EVENT_BUS_NAME;
-import static no.unit.nva.search.BatchIndexingConstants.BATCH_INDEX_EVENT_TOPIC;
 
 public final class EmitEventUtils {
 
@@ -34,7 +31,6 @@ public final class EmitEventUtils {
             .eventBusName(BATCH_INDEX_EVENT_BUS_NAME)
             .source(EventBasedBatchIndexer.class.getName())
             .time(Instant.now())
-            .detailType(BATCH_INDEX_EVENT_TOPIC)
             .detail(importDataRequest.toJsonString())
             .resources(context.getInvokedFunctionArn())
             .build();
